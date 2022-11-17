fcfeb5829a7a2aa03c35c839b572d744f4b18250
dev-rachido <dev.rachido@gmail.com>
2022-11-17 12:00:43 +0100

    Header en banner klaar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import {GlobeAltIcon,MenuIcon,UserCircleIcon,UserIcon,SearchIcon} from '@heroicons/react/solid'
 
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-gray-50 shadow-md p-5">
+    <header className="sticky  top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
 
       {/* Left */}
       <div className=" relative flex items-center h-10 cursor-pointer my-auto">
@@ -15,15 +16,25 @@ function Header() {
         objectPosition='left'
         /> 
       </div>
-    
+      
       {/* center */}
-      <div>
-        <h1>Center</h1>
-      </div>
+      
+    <div className="flex items-center md:border-2
+    rounded-full py-2 md:shadow-sm text-sm text-gray-500">
+      <input type="text" placeholder="Search"
+       className="pl-5 bg-transparent outline-none flex-grow "/>
+       <SearchIcon className="h-7 hidden md:inline-flex bg-red-400 text-white rounded-full p-1 cursor-pointer md:mx-2"/>
+    </div>
+      
       
       {/* Right */}
-      <div>
-        <h1>Right</h1>
+      <div className="flex space-x-4 items-center justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer" >Become a host</p>
+          <GlobeAltIcon className="h-7 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-7 cursor-pointer"/>
+          <UserCircleIcon className="h-7 cursor-pointer"/>
+        </div>
       </div>
     
     </header>
